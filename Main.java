17260b5203dce19f2522d81d78db7bc1b61c201c
Sara <saraguillentorrado@gmail.com>
2024-10-25 00:31:03 +0200

    Tutoría + Refactoring
    - Creados diferentes métodos para cifrar y descifrar
    - Bucle en el main para que el usuario decida cuándo terminar
    - DESCIFRAR NO FUNCIONA (fallo en el cis.read)
    - Corregido el cifrado con Lorenzo
    - Añadida algo de documentación interna
    - FALTAN NOMBRES DE FICHEROS POR TECLADO

--- Main.java
+++ Main.java
@@ -1,46 +1,60 @@
 package algoritmos;
 
-import java.io.*;
-import java.security.SecureRandom;
-import java.security.Security;
-import java.util.List;
-import java.util.ArrayList;
-import librerias.*;
-
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.PBEParameterSpec;
-
+import librerias.Header;
+import librerias.Options;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-// C:\\Users\\celia\\eclipse-workspace-pbd\\cryptoEngineSRT\\src\\algoritmos\\documento.txt
+import javax.crypto.*;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.Security;
 
-public class Main{
-    public final static String fichero = "C:\\Users\\sarag\\Documents\\GitHub\\SRT\\gen\\src\\main\\java\\lab2\\documento.txt";
+
+public class Main {
     public final static int numIteraciones = 100;
 
     public static void main(String[] args) throws Exception {
 
         //Se añade como proveedor de los servicios de seguridad, la librería de Bouncy Castle. Si no, no reconoce aquellas funciones de esta
         Security.addProvider(new BouncyCastleProvider());
+        boolean fin = false;
+        while (!fin) {
 
-        //Se pide al usuario si quiere cifrar o descifrar
-//        boolean esCifrado = elegirCifradoDescifrado();
+            String fichero = solicitarFichero();
 
-        //Se pide al usuario el algoritmo de cifrado
-        String algoritmoCifrado = solicitarAlgoritmoCifrado();
-        
-        Header h = procesarCifrado(algoritmoCifrado);
-        
-        leerFichero(h);
+            boolean esCifrado = elegirCifradoDescifrado();
 
-        
+            String algoritmoCifrado = solicitarAlgoritmoCifrado();
+
+            String contrasena = solicitarContrasena();
+            if (esCifrado) {
+                fichero = "C:\\Users\\sarag\\Documents\\GitHub\\SRT\\prueba1.txt";
+                procesarCifrado(fichero, contrasena, algoritmoCifrado);
+
+            } else {
+                fichero = "C:\\Users\\sarag\\Documents\\GitHub\\SRT\\prueba1.txt.cif";
+                procesarDescifrado(fichero, contrasena);
+            }
+
+            fin = terminar();
+        }
+
+
     }
 
+    /**
+     * Solicita un fichero por teclado //TODO
+     * @return El nombre del fichero
+     */
+    static String solicitarFichero() {
+        String fichero = "C:\\Users\\sarag\\Documents\\GitHub\\SRT\\prueba1.txt.cif"; //TODO
+        return fichero;
+    }
 
     /**
      * Devuelve true si se va a cifrar, false si se va a descifrar
@@ -49,24 +63,43 @@ public class Main{
     static boolean elegirCifradoDescifrado() {
         boolean esCifrado = false;
         char eleccionCifrado = leerTeclado("¿Desea cifrar o descifrar? (C/E): ").charAt(0);
-        while(!eleccionCifradoValida(eleccionCifrado)){
+        while (!eleccionCifradoValida(eleccionCifrado)) {
             eleccionCifrado = leerTeclado("Elige un valor válido (C/E): ").charAt(0);
         }
-        if (esCifrado(eleccionCifrado)){
+        if (esCifrado(eleccionCifrado)) {
             esCifrado = true;
         }
         return esCifrado;
     }
 
     /**
+     * Devuelve true si se va a terminar, false si se va a continuar
+     * @return boolean true si se va a cifrar, false si se va a descifrar
+     */
+    static boolean terminar() {
+        boolean fin = false;
+        boolean valido = false;
+        while (!valido) {
+            char respuesta = leerTeclado("¿Desea terminar la ejecución? (S/N): ").charAt(0);
+            if (respuesta == 'S' || respuesta == 's' || respuesta == 'Y' || respuesta == 'y') {
+                fin = true;
+                valido = true;
+            } else if (respuesta == 'N' || respuesta == 'n') {
+                valido = true;
+            }
+        }
+        return fin;
+    }
+
+    /**
      * Muestra las distintas opciones de algoritmos de cifrado y devuelve el elegido
      * @return String Algoritmo de cifrado elegido
      */
-    static String solicitarAlgoritmoCifrado () {
+    static String solicitarAlgoritmoCifrado() {
         int alCifrado = -1;
         while (alCifrado < 0 || alCifrado >= Options.cipherAlgorithms.length) {
-            for (int i = 0; i<Options.cipherAlgorithms.length; i++) {
-                System.out.println("["+i+"]"+Options.cipherAlgorithms[i]);
+            for (int i = 0; i < Options.cipherAlgorithms.length; i++) {
+                System.out.println("[" + i + "]" + Options.cipherAlgorithms[i]);
             }
             alCifrado = leerIntegerTeclado("Elige un algoritmo de cifrado: ");
         }
@@ -79,16 +112,17 @@ public class Main{
      * @param eleccionCifrado Caracter introducido por el usuario
      * @return boolean true si es una elección válida, false en caso contrario
      */
-    static boolean eleccionCifradoValida(char eleccionCifrado){
+    static boolean eleccionCifradoValida(char eleccionCifrado) {
         return esCifrado(eleccionCifrado) || esDescifrado(eleccionCifrado);
     }
 
     /**
      * Devuelve si el caracter introducido es una elección de cifrado (C o c)
+     *
      * @param eleccionCifrado Caracter introducido por el usuario
      * @return boolean true si es una elección de cifrado, false en caso contrario
      */
-    static boolean esCifrado(char eleccionCifrado){
+    static boolean esCifrado(char eleccionCifrado) {
         return eleccionCifrado == 'C' || eleccionCifrado == 'c';
     }
 
@@ -97,7 +131,7 @@ public class Main{
      * @param eleccionDescifrado Caracter introducido por el usuario
      * @return boolean true si es una elección de cifrado, false en caso contrario
      */
-    static boolean esDescifrado(char eleccionDescifrado){
+    static boolean esDescifrado(char eleccionDescifrado) {
         return eleccionDescifrado == 'E' || eleccionDescifrado == 'e';
     }
 
@@ -145,54 +179,85 @@ public class Main{
     }
 
     /**
-     * Crea un fichero de salida, dado su nombre y un objeto de cifrado inicializado
-     * @param c Objeto de cifrado inicializado
+     * Crea un fichero de salida, dado su nombre y un objeto de cifrado inicializado,
+     * y lo almacena en un fichero.cif
+     * @param c       Objeto de cifrado inicializado
      * @param archivo Nombre del archivo a cifrar
+     * @param h       La cabecera
      * @throws FileNotFoundException Si no se encuentra el archivo
-     * @throws IOException Si hay un error al leer el archivo
-     * @throws Exception Si hay un error al cifrar el archivo
+     * @throws IOException           Si hay un error al leer el archivo
+     * @throws Exception             Si hay un error al cifrar el archivo
      */
-    public static CipherOutputStream escribirFicheroSalida (Cipher c, String archivo, Header h) throws Exception {
-        String salida = archivo+".cif";
+    public static boolean escribirFicheroCifrado(Cipher c, String archivo, Header h) throws Exception {
+        String salida = archivo + ".cif";
 
         FileOutputStream fos = new FileOutputStream(salida); // Abre el flujo de salida desde un fichero
-        OutputStream os = new BufferedOutputStream(fos); // Abre el flujo de salida desde un buffer
-        CipherOutputStream cos = new CipherOutputStream(os,c); // Abre el flujo de salida cifrado
+        CipherOutputStream cos = new CipherOutputStream(fos, c); // Abre el flujo de salida cifrado
 
-        try{
-            FileReader f = new FileReader(archivo); // Lee el archivo
-            BufferedReader b = new BufferedReader(f); // Para poder iterar sobre el archivo
+        boolean exito = false;
+        if (!h.save(fos)) {
+            System.out.println("Error al guardar la cabecera");
+        }
 
-            String cadena = b.readLine();
-
-            while(cadena!=null) {
-                cos.write(cadena.getBytes()); // Escribe en el flujo de salida cifrado
-                cadena = b.readLine();
+        try {
+            int tamano = 8;
+            FileInputStream fis = new FileInputStream(archivo);
+            byte[] contenido = new byte[tamano];
+            while (fis.read(contenido, 0, tamano) > 0) {
+                cos.write(contenido);
             }
-            if(!h.save(os)){
-                System.out.println("Error al guardar la cabecera");
-            }
-
-            f.close();
-            b.close();
+            fis.close();
+            exito = true;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        os.close();
         fos.close();
-        // cos.close();
 
-        return cos;
+        return exito;
+    }
+
+    /**
+     * Descifra un fichero y guarda la salida en un fichero.cla
+     * @param c Objeto de cifrado inicializado
+     * @param archivo El nombre del archivo a descifrar
+     * @return true si se ha descifrado correctamente, y false en caso contrario
+     * @throws Exception
+     */
+    public static boolean escribirFicheroDescifrado(Cipher c, String archivo) throws Exception {
+        String salida = archivo + ".cla";
+
+        FileOutputStream fos = new FileOutputStream(salida); // Abre el flujo de salida desde un fichero
+
+        boolean exito = false;
+        try {
+            int tamano = 8;
+            FileInputStream fis = new FileInputStream(archivo);
+            CipherInputStream cis = new CipherInputStream(fis, c);
+
+            byte[] contenido = new byte[tamano];
+            // TODO ESTO DA FALLO AL LEER LOS ÚLTIMOS BYTES
+            while (cis.read(contenido)!=0) {
+                fos.write(contenido);
+            }
+            fis.close();
+            exito = true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        fos.close();
+
+        return exito;
     }
 
     /**
      * Genera una clave de sesión a partir de una contraseña y un algoritmo
+     *
      * @param contrasena Contraseña a partir de la cual se generará la clave de sesión
-     * @param algoritmo Algoritmo con el que se generará la clave de sesión
+     * @param algoritmo  Algoritmo con el que se generará la clave de sesión
      * @return SecretKey Clave de sesión generada
      * @throws Exception Si el algoritmo no existe o si la clave generada no es válida
      */
-    public static SecretKey generacionClaveSesion(String contrasena, String algoritmo) throws Exception{
+    public static SecretKey generacionClaveSesion(String contrasena, String algoritmo) throws Exception {
         System.out.println("GENERANDO CLAVE DE SESION");
         PBEKeySpec pbeKeySpec = new PBEKeySpec(contrasena.toCharArray());
 
@@ -204,89 +269,91 @@ public class Main{
     }
 
     /**
-     * Configura el algoritmo de cifrado
-     * @param algoritmoCifrado Algoritmo de cifrado a utilizar
-     * @return Cipher Algoritmo de cifrado configurado
-     * @throws Exception Si el algoritmo no existe
+     * Configura el objeto de cifrado y cifra el fichero
+     * @param fichero El fichero a cifrar
+     * @param contrasena La contraseña simétrica
+     * @param algoritmoCifrado El algoritmo de cifrado elegido
+     * @throws Exception
      */
-    public static Header procesarCifrado(String algoritmoCifrado) throws Exception{
-
-        String contrasena = solicitarContrasena();
-
-//        int numIteraciones = leerIntegerTeclado("Introduce el número de iteraciones que desea que haga el algoritmo: ");
+    public static void procesarCifrado(String fichero, String contrasena, String algoritmoCifrado) throws Exception {
 
         Options confAlgoritmo = new Options();
         confAlgoritmo.setCipherAlgorithm(algoritmoCifrado);
-        System.out.println("Conseguir algoritmo de cifrado: instancia");
         Cipher c = Cipher.getInstance(algoritmoCifrado);
 
-        SecureRandom random = SecureRandom.getInstance("DEFAULT", "BC");
-        byte[] salt = random.generateSeed(8);
-
-        PBEParameterSpec pPS = new PBEParameterSpec(salt,numIteraciones);
         SecretKey sKey = generacionClaveSesion(contrasena, algoritmoCifrado);
-//        if(esCifrado)
-            c.init(Cipher.ENCRYPT_MODE,sKey,pPS);
-//        else
-//          c.init(Cipher.DECRYPT_MODE,sKey,pPS);
+        byte[] salt;
+        SecureRandom random = SecureRandom.getInstance("DEFAULT", "BC");
+        salt = random.generateSeed(8);
+
+        PBEParameterSpec pPS = new PBEParameterSpec(salt, numIteraciones);
+        c.init(Cipher.ENCRYPT_MODE, sKey, pPS);
+
 
         Header h = new Header(Options.OP_SYMMETRIC_CIPHER, algoritmoCifrado,
                 Options.authenticationAlgorithms[0], salt);
 
-
-        escribirFicheroSalida(c,fichero, h);
-
-        return h;
-    }
-    
-    public static void leerFichero(Header h) throws Exception {
-        String ficherocif = fichero+".cif";
-        FileInputStream fis = new FileInputStream(ficherocif);
-        if (h.load(fis)){
-            System.out.print("\nOperación: " +h.getOperation());
-            System.out.print(" Algoritmo1: "+h.getAlgorithm1());
-            System.out.print(" Algoritmo2: "+h.getAlgorithm2());
-            System.out.print(" Data: ");
-            byte[] salt = new byte[8];
-            for(byte i=0;i<h.getData().length;i++){
-                salt[i]=h.getData()[i];
-                System.out.print(String.format("0x%h ", h.getData()[i]));
-            }
-
-
-            Cipher c = Cipher.getInstance(h.getAlgorithm1());
-
-            PBEParameterSpec pPS = new PBEParameterSpec(salt,numIteraciones);
-            String contrasena = leerTeclado("Introduce la contraseña: ");
-            SecretKey sKey = generacionClaveSesion(contrasena, h.getAlgorithm1());
-            c.init(Cipher.DECRYPT_MODE,sKey,pPS);
+        if(!escribirFicheroCifrado(c, fichero, h)) {
+            System.out.println("Error al cifrar el fichero");
+        } else {
+            System.out.println("El fichero se cifrado correctamente");
         }
-        else
-            System.out.println("Error en la carga");
-        fis.close();
+
     }
 
     /**
+     * Configura el objeto de descifrado y genera el fichero descifrado
+     * @param fichero El nombre del fichero
+     * @param contrasena La contraseña
+     * @throws Exception
+     */
+    public static void procesarDescifrado(String fichero, String contrasena) throws Exception {
+        Header h = new Header();
+        FileInputStream fis = new FileInputStream(fichero);
+        if (h.load(fis)) {
+            String algoritmoCifrado = h.getAlgorithm1();
+            byte[] salt = h.getData();
+            Options confAlgoritmo = new Options();
+            confAlgoritmo.setCipherAlgorithm(algoritmoCifrado);
+            Cipher c = Cipher.getInstance(algoritmoCifrado);
+            SecretKey sKey = generacionClaveSesion(contrasena, algoritmoCifrado);
+            PBEParameterSpec pPS = new PBEParameterSpec(salt, numIteraciones);
+            c.init(Cipher.DECRYPT_MODE, sKey, pPS);
+
+            if(!escribirFicheroDescifrado(c, fichero)) {
+                System.out.println("Error al descifrar el fichero");
+            } else {
+                System.out.println("El fichero se ha descifrado correctamente");
+            }
+        }
+
+
+    }
+
+
+    /**
      * Comprueba si una contraseña es segura
+     *
      * @param contrasena Contraseña a comprobar
      * @return boolean True si la contraseña es segura, false en caso contrario
      */
-    public static boolean contrasenaSegura(String contrasena){
+    public static boolean contrasenaSegura(String contrasena) {
 
         return contrasena.length() >= 8;
     }
 
     /**
      * Solicita una contraseña segura por teclado, y solicita de nuevo la contraseña
+     *
      * @return La contraseña introducida por el usuario
      */
     public static String solicitarContrasena() {
         String contrasena = "";
-        while(!contrasenaSegura(contrasena)) {
+        while (!contrasenaSegura(contrasena)) {
             contrasena = leerTeclado("Introduce la contraseña: ");
         }
         String repetida = "";
-        while(!contrasena.equals(repetida)){
+        while (!contrasena.equals(repetida)) {
             repetida = leerTeclado("Repite la primera contraseña: ");
         }
         return contrasena;
